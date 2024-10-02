2f92633ba6d41a664c5a38830bc09815eedf773c
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2024-10-02 14:26:05 +0200

    Test JS types

---
+++ src/app1.js
@@ -0,0 +1,9 @@
+import { Sms } from '@sinch/sdk-core';
+
+/** @type {Sms.GroupResponse[]} */
+const groupsList1 = [];
+
+/** @type {Sms.Group[]} */
+const groupsList2 = [];
+
+console.log(groupsList1, groupsList2);
---
+++ src/app2.js
@@ -0,0 +1,14 @@
+import { Sms } from '@sinch/sdk-core';
+
+/** @type {Sms.GroupResponse[]} */
+const groupsList1 = [
+  {
+    id: 'tt',
+    idd: 'gg'
+  }
+];
+
+/** @type {Sms.Group[]} */
+const groupsList2 = [];
+
+console.log(groupsList1, groupsList2);
